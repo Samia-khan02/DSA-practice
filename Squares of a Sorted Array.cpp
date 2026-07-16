a008a517cd73f3c3a940d908fe4dc0bcec69ef1d
Samia khan <samia.2023.khan@gmail.com>
2026-07-16 10:54:35 +0530

    Update Squares of a Sorted Array.cpp

--- Squares of a Sorted Array.cpp
+++ Squares of a Sorted Array.cpp
@@ -1,5 +1,19 @@
 class Solution {
 public:
+/* APPROACH 1:
+        int n = nums.size();
+
+        for(int i = 0; i < n; i++) {
+            nums[i] = nums[i] * nums[i];
+        }
+
+        sort(nums.begin(), nums.end());
+
+        return nums;
+    }
+}; */
+
+//APPROACH 2
     vector<int> sortedSquares(vector<int>& nums) {
         int n = nums.size();
         vector < int > ans(n);
