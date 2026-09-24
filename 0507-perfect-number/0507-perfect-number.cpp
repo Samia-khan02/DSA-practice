dc96f62c5407d3908577a928c2e6f419afb00972
Samia khan <samia.2023.khan@gmail.com>
2026-09-24 10:00:24 +0530

    Time: 0 ms (100%), Space: 7.8 MB (55.82%) - LeetHub

---
+++ 0507-perfect-number/0507-perfect-number.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool checkPerfectNumber(int n) {
+        if( n <= 1){
+            return false;
+        }
+        int sum = 1;
+        // for( int i = 2; i<n ; i++ ){
+        //     if( n%i == 0 ){
+        //          sum += i;
+        //     }
+        // }
+        for( int i = 2 ; i*i<= n ; i++ ){
+            if( n%i == 0 ){
+                sum += i;
+                if( n/i != i ){
+                    sum += n/i;
+                }
+            }
+        }
+        return sum == n;
+    }
+};
