c04cb3af8d1b8fc17ea471cc40dd162e5b8845c1
Samia khan <samia.2023.khan@gmail.com>
2026-09-26 16:03:43 +0530

    Time: 0 ms (100%), Space: 27.3 MB (51.72%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.cpp
+++ 0344-reverse-string/0344-reverse-string.cpp
@@ -1,12 +1,20 @@
 class Solution {
 public:
     void reverseString(vector<char>& s) {
-        int left = 0 , 
-        right = s.size()-1 ;
-        while( left < right ){
-            swap( s[left] , s[right]);
-            left++;
-            right--;
+    // TWO POINTER
+        // int left = 0 , 
+        // right = s.size()-1 ;
+        // while( left < right ){
+        //     swap( s[left] , s[right]);
+        //     left++;
+        //     right--;
+        // }
+    // SINGLE POINTER
+        int i = 0 ;
+        int n = s.size();
+        while ( i < n/2){
+            swap( s[i] , s[n-i-1] );
+            i++;
         }
     }
 };
